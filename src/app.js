6d0e4fc6bd8ff0e00d55735de6377cefc0b72d02
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-04-14 13:55:58 -0300

    get+post participants

---
+++ src/app.js
@@ -0,0 +1,58 @@
+import express from "express"
+import cors from "cors"
+import { MongoClient, ObjectId } from "mongodb"
+import dotenv from "dotenv"
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+try {
+    await mongoClient.connect()
+} catch (err) {
+    console.log(err.message)
+}
+const db = mongoClient.db()
+
+app.post("/participants", async (req, res) => {
+    const { name } = req.body
+    if (typeof (name) !== "string" || name.length === 0) return res.sendStatus(422)
+    const result = await db.collection("participants").findOne({ name: name })
+    if (result) return res.sendStatus(409)
+
+    const participante =
+    {
+        name: name,
+        lastStatus: Date.now()
+    }
+
+    const mensagem =
+    {
+        from: name,
+        to: "Todos",
+        text: "entra na sala...",
+        type: "status",
+        time: dayjs //ARRUMAR HH:mm:ss
+    }
+
+    try {
+        await db.collection("participants").insertOne(participante)
+        await db.collection("messages").insertOne(mensagem)
+
+        res.sendStatus(201)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+
+app.get("participants", async (req, res) => {
+    try {
+        const result = await db.collection("participants").find().toArray()
+        res.send(result)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
+app.listen(5000)
